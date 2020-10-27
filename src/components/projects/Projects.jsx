2c04284aab7a5457542f315c3594879fb25cfde1
winbigcoms <bigcoms6290@gmail.com>
2020-10-27 17:40:47 +0900

    Feat '

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Projects(){
+  return(
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <h3>EOTTEA CINEMA</h3>
+          <div>
+            <a href="https://eottaecinema.herokuapp.com/">
+              EOTTEA CINEMA
+            </a>
+          </div>
+          <div>
+            <ul>
+              <li>프로젝트 발표 PPT</li>  
+              <li>github</li>  
+              <li></li>  
+            </ul>  
+          </div>
+        </li>
+      </ul>
+    </section>
+  )
+}
